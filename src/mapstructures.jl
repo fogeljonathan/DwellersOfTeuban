4467c5fc91fec2e26c8a12631e7e5bc213ccc551
Jonathan Fogel <77300605+fogeljonathan@users.noreply.github.com>
2022-11-17 09:45:25 -0500

    intiial commit mapstructures.jl - added pathnode and structurenode frameworks

---
+++ src/mapstructures.jl
@@ -0,0 +1,26 @@
+"""
+    Describes a spot on a given board on which a road can possibly be built. 
+
+    Stores data on whether or not a player is occupying it, and, if so, who.
+
+    Defaults to an empty node.
+"""
+mutable struct PathNode
+    owner #inits as nothing -- CURRENTLY NOT RESTRICTED TO PLAYER TYPING FOR EMPTY INIT PURPOSES
+end
+PathNode() = PathNode(nothing)
+
+"""
+    Describes a spot on a given board on which a settlement/city exists or can possibly be built. 
+
+    Stores data on whether or not a player is occupying it, and what structure is on it. Stores info on whether or not it is on a port. Also stores info on surrounding pathnodes.
+
+    Defaults to an empty, portless, node.
+"""
+mutable struct StructureNode
+    structuretype #inits as nothing
+    owner #inits as nothing -> ::player  -- CURRENTLY NOT RESTRICTED TO PLAYER TYPING FOR EMPTY INIT PURPOSES
+    paths::Array{PathNode}
+    port::Bool #defaults to false
+end
+StructureNode() = StructureNode(nothing, nothing, [PathNode(), PathNode(), PathNode()], false)
